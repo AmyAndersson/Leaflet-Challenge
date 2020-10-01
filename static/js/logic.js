4265660eeb8d9d4a5ce28062e7c5d85a920c8c8b
AmyAndersson <66049569+AmyAndersson@users.noreply.github.com>
2020-10-01 15:54:45 +0800

    finsihed step 1

--- static/js/logic.js
+++ static/js/logic.js
@@ -2,7 +2,7 @@ var AusCoords = [-25.2744, 133.775];
 var mapZoomLevel = 3;
 
 // Create the createMap function
-var myMap = L.map("mapid", {
+var myMap = L.map("map-id", {
   center: AusCoords,
   zoom: mapZoomLevel,
 });
@@ -20,30 +20,66 @@ L.tileLayer(
       accessToken: API_KEY,
     }
   ).addTo(myMap);
+
   // Load in geojson data
-  var EQdata =
-  "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+var EQdata = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+
+function Colourfun(depth) {
+  switch (true) {
+    case depth > 20:
+      return "#ea2c2c";
+    case depth > 15:
+      return "#ea822c";
+    case depth > 10:
+      return "#ee9c00";
+    case depth > 5:
+      return "#eecc00";
+    case depth > 1:
+      return "#d4ee00";
+    default:
+      return "#98ee00";
+  }; 
+};
+
+
 
 
 // Grab data with d3
-d3.json(EQdata, (Earthquakes) => {
+d3.json(EQdata, (earthquakesdata) => {
+  console.log(EQdata);
+  var earthquake_list = [];
+  var feature = earthquakesdata.features
 
+  // console.log(feature[0].geometry.coordinates);
+  
+
+  // for each function to populate earthquake markers
+      feature.forEach((feature) => {
+        var EQMarker = L.circle([feature.geometry.coordinates[1], feature.geometry.coordinates[0]],{
+          opacity: 1,
+          fillOpacity: 1,
+          fillColor: Colourfun(feature.geometry.coordinates[2]),
+          color: "#000000",
+          radius: feature.properties.mag *20000,
+          stroke: true,
+          weight: 0.5
+        })      
+        .bindPopup(
+          "<strong>Earthquake ID: </strong>" +
+          feature.id +
+            "<br><strong>Depth: </strong><br> " +
+            feature.geometry.coordinates[2] +
+            "<br><strong>Mag: </strong><br> " +
+            feature.properties.mag  
+        );
+
+        earthquake_list.push(EQMarker);
+        // console.log(earthquake_list)
+
+        });
  
-  var earthquake = Earthquakes.features;
-  console.log(earthquake);
-  earthquake.forEach((EQ) => {
-    var EQMarkers_list = [];
-    var EQMarker = L.marker([EQ.geometry.coordinates[1], EQ.geometry.coordinates[0]]);
-      EQMarker.bindPopup(
-      "<strong>Earthquake ID: </strong>" +
-      EQ.geometry.id +
-        "<br><strong>Depth: </strong><br> " +
-        EQ.geometry.id +
-        "<br><strong>Mag: </strong><br> " +
-        EQ.properties.mag
-    );
-    EQMarkers_list.push(EQMarker);
-  });
-  var EQlayer = L.layerGroup(EQMarkers_list);
-  EQlayer.addTo(myMap);
+
+    var EQLayer = L.layerGroup(earthquake_list); 
+    EQLayer.addTo(myMap);
   });
