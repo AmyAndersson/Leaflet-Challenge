b7a8770b7f63f3fdb0bb0fa6de55534d4bfeea36
AmyAndersson <66049569+AmyAndersson@users.noreply.github.com>
2020-10-02 19:38:14 +0800

    updated with legend

--- Step 2/static/js/logic.js
+++ Step 2/static/js/logic.js
@@ -2,7 +2,11 @@ var AusCoords = [-25.2744, 133.775];
 var mapZoomLevel = 3;
 
 // Create the createMap function
-
+var myMap = L.map("map-id", {
+  center: AusCoords,
+  zoom: mapZoomLevel,
+  layers: satellite
+});
 
 // Create the tile layer that will be the background of our map
 var satellite = L.tileLayer(
@@ -38,7 +42,6 @@ function Colourfun(depth) {
       return "#98ee00";
   }; 
 };
-
 
 
 
@@ -81,49 +84,39 @@ d3.json(EQdata, (earthquakesdata) => {
     EQLayer.addTo(myMap);
 
 
-//Creating other base layers
-var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-  tileSize: 512,
-  maxZoom: 18,
-  zoomOffset: -1,
-  id: "mapbox/streets-v11",
-  accessToken: API_KEY
-});
-
-var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-  maxZoom: 18,
-  id: "dark-v10",
-  accessToken: API_KEY
-});
-
-// Create a baseMaps object
-var baseMaps = {
-  "Street Map": streetmap,
-  "Dark Map": darkmap
-};
-
 
 var myMap = L.map("map-id", {
   center: AusCoords,
   zoom: mapZoomLevel,
-  layers: [satellite, EQLayer]
+  layers: satellite
 });
-// Create an overlay object
-// var overlayMaps = {
-
-// };
-
-// Pass our map layers into our layer control
-// Add the layer control to the map
-L.control.layers(baseMaps, {
-  collapsed: false
-}).addTo(myMap);
 
 
+// // Pass our map layers into our layer control
+// // Add the layer control to the map
+// L.control.layers(baseMaps, overlayMaps, {
+//   collapsed: false
+// }).addTo(myMap);
 
-  // // Adding legend to the map
-  // legend.addTo(myMap);
 
-  });
+var legend = L.control({ position: "bottomright" });
+legend.onAdd = function (leg) {
+  var div = L.DomUtil.create("div", "info legend");
+  div.innerHTML += "<h4>Earthquake Depth</h4>";
+  div.innerHTML +=
+    '<i style="background: #99FF33"></i><span> -10 to 10</span><br>';
+  div.innerHTML +=
+    '<i style="background: #CCFF66"></i><span>10 to 30</span><br>';
+  div.innerHTML +=
+    '<i style="background: #FFCC33"></i><span>30 to 50</span><br>';
+  div.innerHTML +=
+    '<i style="background: #FF9900"></i><span>50 to 70</span><br>';
+  div.innerHTML +=
+    '<i style="background: #FF6600"></i><span>70 to 90</span><br>';
+  div.innerHTML += '<i style="background: #FF0000"></i><span>90 +</span><br>';
+
+  return div;
+};
+
+legend.addTo(myMap);
+});
